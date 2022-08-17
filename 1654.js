86b50711f493c1a944866bc89c6454ff5debd78f
seongwon <87584033+Magiof@users.noreply.github.com>
2022-08-17 21:37:49 +0900

    ADD: 1654

---
+++ 1654.js
@@ -0,0 +1,9 @@
+let fs = require('fs');
+let input = fs.readFileSync('example.txt').toString().split('\n');
+const [n, t] = input.shift().split(' ');
+const lines = input.map(Number);
+function solution(n, t, arr) {
+  let answer = 0;
+  let lt = 1;
+  let rt = Math.min(...arr);
+}
