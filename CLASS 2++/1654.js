59e4d58a47b068a81c2e2ece4c88ede5c741e3bb
magiof <tjddnjs7559@gmail.com>
2022-08-18 01:40:30 +0900

    FIX: 1654

--- CLASS 2++/1654.js
+++ CLASS 2++/1654.js
@@ -1,9 +1,22 @@
 let fs = require('fs');
-let input = fs.readFileSync('1654.txt').toString().split('\n');
-const [n, t] = input.shift().split(' ');
+let input = fs.readFileSync('1654.txt').toString().trim().split('\n');
+const [n, t] = input.shift().split(' ').map(Number);
 const lines = input.map(Number);
-function solution(n, t, arr) {
+function solution(t, arr) {
   let answer = 0;
-  let lt = 1;
-  let rt = Math.min(...arr);
+  let lt = 0;
+  let rt = Math.max(...arr);
+  while (lt <= rt) {
+    let mid = parseInt((lt + rt) / 2);
+    let sum = arr.map(e => parseInt(e / mid)).reduce((a, b) => a + b)
+    if (sum === t) {
+      answer = mid;
+      break;
+    }
+    else if (sum < t) rt = mid
+    else if (sum > t) lt = mid
+    console.log(answer, lt, mid, rt, sum, arr)
+  }
+  return answer
 }
+console.log(solution(t, lines))
