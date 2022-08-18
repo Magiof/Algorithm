c18d9191a8062d18b04523e961ca9fa1231011b1
seongwon <87584033+Magiof@users.noreply.github.com>
2022-08-19 07:34:48 +0900

    ADD: 1929

---
+++ step/1929.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const [N,M] = fs.readFileSync('1929.txt').toString().trim().split(' ').map(Number);
+let prime = Array(M + 1).fill(true);
+prime[0] = prime[1] = false;
+
+function solution(N,M) {
+  for (let i = 2; i <= Math.ceil(Math.sqrt(M)); i++) {
+    if(prime[i]) {
+      let j = 2; 
+      while(i * j <= M) { 
+        prime[i * j] = false; 
+        j++;  
+      }
+    }
+  }
+  
+  let result = []; 
+  for(let i = N; i <= M; i++) { 
+    if(prime[i]) { 
+		result.push(i);
+    }
+  }
+  return result.join('\n')
+}
+
+console.log(solution(N,M))
