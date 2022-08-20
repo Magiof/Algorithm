7681c245149e10a8169984f2352bb1a28979da32
seongwon <87584033+Magiof@users.noreply.github.com>
2022-08-20 21:31:50 +0900

    ADD: 주석 추가

--- step/1929.js
+++ step/1929.js
@@ -3,6 +3,7 @@ const [N,M] = fs.readFileSync('1929.txt').toString().trim().split(' ').map(Numbe
 let prime = Array(M + 1).fill(true);
 prime[0] = prime[1] = false;
 
+// 에라토스테네스의 체
 function solution(N,M) {
   for (let i = 2; i <= Math.ceil(Math.sqrt(M)); i++) {
     if(prime[i]) {
