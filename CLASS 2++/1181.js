e8c4f76f64ffc64495f5b881d90e5ec75cfa0102
seongwon <87584033+Magiof@users.noreply.github.com>
2022-08-20 21:31:40 +0900

    ADD: 1181

---
+++ CLASS 2++/1181.js
@@ -0,0 +1,25 @@
+let fs = require('fs');
+let input = fs
+  .readFileSync('1181.txt')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((e) => e.replaceAll('\r', ''));
+
+input.shift();
+
+let sorted = input.sort((a, b) => {
+  if (a.length === b.length) {
+    for(let i=0; i<a.length; i++){
+        let A=a.charCodeAt(i)
+        let B=b.charCodeAt(i)
+        if(A<B) return A-B
+        if(A>B) return A-B
+    }
+  } else return a.length - b.length;
+});
+
+const set = new Set(sorted)
+const answer = [...set]
+
+console.log(answer.join('\n'));
