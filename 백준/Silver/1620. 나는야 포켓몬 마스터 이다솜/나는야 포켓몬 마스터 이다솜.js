d3f2dcbd6a638f7a274e1b197484d9ca7c30b3a3
황성원 <87584033+Magiof@users.noreply.github.com>
2022-08-26 15:10:52 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 348 ms, Memory: 69536 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.js
@@ -0,0 +1,25 @@
+let fs = require('fs');
+let input = fs
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((e) => e.replaceAll('\r', ''));
+const [N, M] = input.shift().split(' ').map(Number);
+const arr = input.slice(0, N);
+const questions = input.slice(N);
+
+function solution(arr, questions) {
+  let answer = '';
+
+  const map = new Map(arr.map((e, i) => [e, i + 1]));
+
+  for (let x of questions) {
+    Number.isNaN(x*1)   // 숫자형으로 변환하지 않으면 map.get(x)=== undefind
+      ? (answer += map.get(x).toString())
+      : (answer += arr[x - 1]);
+    answer += '\n';
+  }
+  return answer;
+}
+console.log(solution(arr, questions));
