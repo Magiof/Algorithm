f9c78343bd96b709eaa84e9fd05c1ab124216fdb
황성원 <87584033+Magiof@users.noreply.github.com>
2022-09-09 14:45:18 +0900

    [Silver III] Title: 통계학, Time: 684 ms, Memory: 73796 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.js
@@ -0,0 +1,53 @@
+let fs = require('fs');
+let input = fs
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n')
+  .map(Number);
+const n = input.shift();
+input = input.sort((a, b) => a - b);
+
+//평균
+function getAverage(arr) {
+  return Math.round(arr.reduce((a, b) => a + b) / arr.length);
+}
+
+// 중앙값
+function getMedian(arr) {
+  return arr[(arr.length - 1) / 2];
+}
+
+// 최빈값
+function getMode(arr) {
+  const values = [];
+  const modes = [];
+  const map = new Map();
+  for (const num of input) {
+    map.has(num) ? map.set(num, map.get(num) + 1) : map.set(num, 1);
+  }
+
+  for (const value of map.values()) {
+    values.push(value);
+  }
+  const countMode = Math.max(...values);
+
+  for (const [key, value] of map) {
+    if (value === countMode) modes.push(key);
+  }
+  const mode = modes.length === 1 ? modes[0] : modes[1];
+
+  return mode;
+}
+
+// 범위
+function getRange(arr) {
+  const range = arr[arr.length - 1] - arr[0];
+  return range;
+}
+
+console.log(
+  `${getAverage(input)}\n${getMedian(input)}\n${getMode(input)}\n${getRange(
+    input
+  )}`
+);
