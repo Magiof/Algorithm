934d80ff115d10ab94191e85f7b3d1ceae0a5533
magiof <tjddnjs7559@gmail.com>
2022-09-09 18:29:31 +0900

    ADD: 2231

---
+++ CLASS 2++/2231.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const N = fs.readFileSync('2231.txt').toString() * 1;
+
+function solution(N) {
+  let answer = 0;
+  for (let i = 0; i <= N; i++) {
+    let tempNumber = 0;
+    let sumDisits = 0;
+    let n = i;
+    do {
+      sumDisits += n % 10;
+      n = Math.floor(n / 10);
+    } while (n > 0);
+
+    tempNumber = i + sumDisits;
+    if (tempNumber === N) {
+      answer = i;
+      break;
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution(N));
