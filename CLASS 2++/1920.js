6e30142c318a4b6ea808df4693c406f28f6262b1
magiof <tjddnjs7559@gmail.com>
2022-09-09 13:19:15 +0900

    ADD: 1920

---
+++ CLASS 2++/1920.js
@@ -0,0 +1,40 @@
+let fs = require('fs');
+let [N, arr1, M, arr2] = fs
+  .readFileSync('1920.txt')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((e) => e.split(' '));
+const list = arr1.map(Number).sort((a,b) => (a-b));
+const target = arr2.map(Number);
+
+function solution(list, target) {
+  let answer = '';
+  loop1: for (const numToCheck of target) {
+    let lt = 0;
+    let rt = list.length - 1;
+    if (numToCheck < list[lt] || numToCheck > list[rt]) {
+      answer += '0\n';
+      continue;
+    }
+
+    loop2: while (lt <= rt) {
+      let mid = Math.floor((lt + rt) / 2);
+
+      if (numToCheck > list[mid]) {
+        lt = mid + 1;
+      } else if (numToCheck < list[mid]) {
+        rt = mid - 1;
+      } else if (numToCheck === list[mid]) {
+        answer += '1\n';
+        continue loop1;
+      }
+    }
+
+    answer += '0\n';
+  }
+
+  return answer.substring(0, answer.length - 1);
+}
+
+console.log(solution(list, target));
