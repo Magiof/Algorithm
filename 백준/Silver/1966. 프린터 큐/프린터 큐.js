cdb7003387d9692ed706888cfd591c268cf44a92
황성원 <87584033+Magiof@users.noreply.github.com>
2022-09-14 21:05:45 +0900

    [Silver III] Title: 프린터 큐, Time: 184 ms, Memory: 10028 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.js
@@ -0,0 +1,27 @@
+let fs = require('fs');
+let input = fs.readFileSync('dev/stdin').toString().trim().split('\n');
+const n = input[0];
+
+function solution(target, arr) {
+  let cnt = 1;
+  let queue = [...arr];
+  while (1) {
+    if (queue[0] === Math.max(...queue)) {
+      if (target === 0) return cnt;
+      else if (target !== 0) {
+        queue.shift();
+        cnt++;
+        target--;
+      }
+    } else {
+      target === 0 ? (target = queue.length - 1) : target--;
+      queue.push(queue.shift());
+    }
+  }
+}
+
+for (let i = 1; i <= n; i++) {
+  let target = input[i * 2 - 1].split(' ').map(Number)[1];
+  let arr = input[i * 2].split(' ').map(Number);
+  console.log(solution(target, arr));
+}
