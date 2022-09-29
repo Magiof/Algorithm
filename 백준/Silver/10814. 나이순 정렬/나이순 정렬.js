5b5714ce3b60812280657c0ecab0f347810978f2
황성원 <87584033+Magiof@users.noreply.github.com>
2022-09-29 20:14:41 +0900

    [Silver V] Title: 나이순 정렬, Time: 1868 ms, Memory: 33496 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -0,0 +1,17 @@
+let fs = require('fs');
+let [_, ...input] = fs
+  .readFileSync('dev/stdin')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((e) => e.replaceAll('\r', ''));
+
+function solution(arr) {
+  let answer;
+  answer = arr.sort((a, b) => {
+    if (a.split(' ')[0] === b.split(' ')[0]) return;
+    else return a.split(' ')[0] - b.split(' ')[0];
+  });
+  return answer.join('\n')
+}
+console.log(solution(input))
