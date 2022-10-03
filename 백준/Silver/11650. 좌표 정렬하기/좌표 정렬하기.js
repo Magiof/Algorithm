f0a82d3e9a4fa2afef8734e36e6a6adf5ef5f695
황성원 <87584033+Magiof@users.noreply.github.com>
2022-10-03 20:40:52 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2772 ms, Memory: 47800 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,18 @@
+let fs = require('fs');
+let [_, ...points] = fs
+  .readFileSync('dev/stdin')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((e) => e.replaceAll('\r', ''));
+
+function solution(arr) {
+  const answer = arr.sort((a, b) => {
+    if (a.split(' ')[0] === b.split(' ')[0])
+      return a.split(' ')[1] - b.split(' ')[1];
+    else return a.split(' ')[0] - b.split(' ')[0];
+  });
+  return answer;
+}
+
+console.log(...solution(points));
