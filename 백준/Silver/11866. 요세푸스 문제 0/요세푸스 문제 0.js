ad3fa6073fa55c6a7aae1e26bb277aa4da1f778f
황성원 <87584033+Magiof@users.noreply.github.com>
2022-10-03 22:41:15 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 212 ms, Memory: 11836 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.js
@@ -0,0 +1,16 @@
+let fs = require('fs');
+let [n, k] = fs.readFileSync('dev/stdin').toString().split(' ').map(Number);
+
+let people = new Array(n).fill(1).map((v, i) => i + 1);
+
+function solution(arr) {
+  let answer = '<';
+  while (arr.length > 0) {
+    for (let i = 0; i < k - 1; i++) {
+      people.push(people.shift());
+    }
+    answer += `${people.shift()}, `;
+  }
+  return answer;
+}
+console.log(solution(people).slice(0, -2) + '>');
