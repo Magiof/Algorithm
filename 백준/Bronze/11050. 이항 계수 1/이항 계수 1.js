cd1dbd4adea83f4e0a07b876eac887b4eb217a4b
황성원 <87584033+Magiof@users.noreply.github.com>
2022-10-03 20:30:33 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.js
@@ -0,0 +1,18 @@
+let fs = require('fs');
+let [n, k] = fs.readFileSync('dev/stdin').toString().split(' ').map(Number);
+
+function factoral(number) {
+  let answer = 1;
+  if (number === 0) return answer;
+  for (let i = 1; i <= number; i++) {
+    answer *= i;
+  }
+  return answer;
+}
+
+function solution(n, k) {
+  const answer = factoral(n) / (factoral(n - k) * factoral(k));
+  return answer;
+}
+
+console.log(solution(n, k));
