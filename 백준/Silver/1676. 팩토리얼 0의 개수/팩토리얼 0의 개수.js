a7cf72e0cdb733efb7032f806df85e7cb97ab191
황성원 <87584033+Magiof@users.noreply.github.com>
2022-10-08 14:05:04 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 128 ms, Memory: 9620 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const N = fs.readFileSync('dev/stdin').toString() * 1;
+
+//5의 갯수가 곧 0의 갯수이지 않을까..?
+function getCountFive(currentNum) {
+  let n = currentNum;
+  let cnt = 0;
+  if (!n) return 0
+  while (n / 5 === parseInt(n / 5)) {
+    n /= 5;
+    cnt++;
+  }
+  return cnt;
+}
+
+function solution(N) {
+  let answer = 0;
+  for (let i = 0; i <= N; i++) {
+    answer += getCountFive(i)
+  }
+  return answer
+}
+
+console.log(solution(N))
