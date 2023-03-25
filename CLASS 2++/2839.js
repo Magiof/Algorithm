b379ae84f6fed83ea82038554cc4ecd7dc343973
magiof <tjddnjs7559@gmail.com>
2023-03-26 00:59:57 +0900

    chore: solve 2839

---
+++ CLASS 2++/2839.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = Number(fs.readFileSync("2839.txt").toString());
+
+function solve() {
+  let count = 0;
+  let weight = input
+
+  while (true) {
+    if (weight % 5 === 0) {
+      count += weight / 5; 
+      break;
+    } else if (weight < 3) {
+      count = -1;
+      break;
+    }
+    weight -= 3;
+    count++;
+  }
+
+  return count
+}
+
+console.log(solve());
