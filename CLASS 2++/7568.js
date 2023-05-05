8ec6a88c528caf68059b616e057b171a401f5f5f
magiof <tjddnjs7559@gmail.com>
2023-05-06 04:03:21 +0900

    solve: 7568

---
+++ CLASS 2++/7568.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+let [_, ...input] = fs
+  .readFileSync('7568.txt')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((e) => e.split(' ').map(Number));
+
+function solution(people) {
+  const result = [];
+  for (let i = 0; i < people.length; i++) {
+    const target = people[i];
+    let rank = 1;
+    for (let j = 0; j < people.length; j++) {
+      if (i === j) continue;
+      const comparison = people[j];
+      if (target[0] < comparison[0] && target[1] < comparison[1]) rank++;
+    }
+    result.push(rank);
+  }
+  const answer = result.join(' ');
+
+  return answer;
+}
+
+console.log(solution(input));
