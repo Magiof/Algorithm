db01449dd0e0d8f7d0cd89fc0d5693dab5dabf4b
황성원 <87584033+Magiof@users.noreply.github.com>
2023-05-06 02:12:06 +0900

    [Silver II] Title: 나무 자르기, Time: 1096 ms, Memory: 132220 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+let [target, trees] = fs.readFileSync('dev/stdin').toString().trim().split('\n');
+target = Number(target.split(' ')[1]);
+trees = trees.split(' ').map(Number);
+
+function cutTrees(height, trees) {
+  let result = 0;
+  trees.forEach((tree) => {
+    if (tree > height) {
+      result += tree - height;
+    }
+  });
+
+  return result;
+}
+
+function solution(target, trees) {
+  let [max, min, answer] = [Math.max(...trees), 0, 0];
+  while (min <= max) {
+    const mid = Math.floor((min + max) / 2);
+    const result = cutTrees(mid, trees);
+
+    if (result === target) return mid;
+    else if (result >= target) {
+      if (mid > answer) answer = mid;
+      min = mid + 1;
+    } else {
+      max = mid - 1;
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution(target, trees));
