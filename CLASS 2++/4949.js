8a756b055391ffb000b87172edba484dcce562fe
magiof <tjddnjs7559@gmail.com>
2023-05-06 17:04:36 +0900

    solve: 4949

---
+++ CLASS 2++/4949.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+let input = fs
+  .readFileSync('4949.txt')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((e) => e.replace('\r', ''));
+
+function solution(sentences) {
+  const results = [];
+  loop1: for (const sentence of sentences) {
+    const stack = [];
+    if (sentence === '.') {
+      const answer = results
+        .map((result) => (result ? 'yes' : 'no'))
+        .join(`\n`);
+      return answer;
+    }
+    loop2: for (const letter of sentence) {
+      if (letter === '(' || letter === '[') stack.push(letter);
+      if (letter === ')') {
+        if (stack[stack.length - 1] === '(') stack.pop();
+        else {
+          results.push(false);
+          continue loop1;
+        }
+      }
+      if (letter === ']') {
+        if (stack[stack.length - 1] === '[') stack.pop();
+        else {
+          results.push(false);
+          continue loop1;
+        }
+      }
+    }
+    results.push(!stack.length);
+  }
+}
+
+console.log(solution(input));
