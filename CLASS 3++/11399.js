5100470911f1b8cf207a5eff85352479ce753190
magiof <tjddnjs7559@gamil.com>
2023-05-14 20:07:40 +0900

    solve: 11399

---
+++ CLASS 3++/11399.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const [_, ...input] = fs
+  .readFileSync('11399.txt')
+  .toString()
+  .trim()
+  .split('\n');
+const times = input[0].split(' ').map(Number);
+
+function solve(times) {
+  const length = times.length;
+  const sortedTimes = times.sort((a, b) => a - b);
+  let answer = 0;
+  for (let i = 0; i < length; i++) {
+    const add = sortedTimes[i] * (length - i);
+    answer += add;
+  }
+
+  return answer;
+}
+
+console.log(solve(times));
