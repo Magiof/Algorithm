59d3e0e535d1884f771413402c0b894b34a68b11
황성원 <87584033+Magiof@users.noreply.github.com>
2023-05-26 13:23:14 +0900

    [Silver IV] Title: 동전 0, Time: 192 ms, Memory: 9580 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const [NK, ...input] = fs
+  .readFileSync('dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const [N, K] = NK.split(' ').map(Number);
+const coins = input.map(Number);
+
+function solve(coins, K) {
+  let count = 0;
+
+  for (let i = coins.length - 1; i >= 0; i--) {
+    const coin = coins[i];
+    if (K >= coin) {
+      count += Math.floor(K / coin);
+      K %= coin;
+    }
+    if (K === 0) {
+      break;
+    }
+  }
+
+  return count;
+}
+console.log(solve(coins, K));
