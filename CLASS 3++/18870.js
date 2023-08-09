f19206d008b82c725a2aa81ab2539f8a2ad2e32a
seongwon-maumfood <sw9384@balance.site>
2023-08-09 17:46:07 +0900

    solve: 18870

---
+++ CLASS 3++/18870.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const [_, input] = fs.readFileSync('18870.txt').toString().trim().split('\n');
+const numbers = input.split(' ').map(Number);
+
+function solve(numbers) {
+  const sorted = Array.from(new Set(numbers)).sort((a, b) => a - b);
+
+  const map = new Map(sorted.map((val, idx) => [val, idx]));
+
+  return numbers.map((number) => map.get(number)).join(' ');
+}
+
+console.log(solve(numbers));
