516e4f4e933bfd79bab73783b405f5888a8a44bc
황성원 <87584033+Magiof@users.noreply.github.com>
2023-08-09 16:12:44 +0900

    [Silver III] Title: 터렛, Time: 112 ms, Memory: 9448 KB -BaekjoonHub

---
+++ 백준/Silver/1002. 터렛/터렛.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const [_, ...input] = fs.readFileSync('dev/stdin').toString().trim().split('\n');
+const cases = input.map((_case) => _case.split(' ').map(Number));
+
+function solve(cases) {
+  const answers = cases.map((_case) => {
+    const [x1, y1, r1, x2, y2, r2] = _case;
+    const distance = calcHypotenuse(Math.abs(x2 - x1), Math.abs(y2 - y1));
+    if (distance === 0 && r1 === 0 && r2 === 0) return 1;
+    if (distance === 0 && r1 === r2) return -1;
+    if (distance > r1 + r2) return 0;
+    if (distance === r1 + r2) return 1;
+    if (distance < Math.abs(r1 - r2)) return 0;
+    if (distance === Math.abs(r1 - r2)) return 1;
+    if (distance !== 0 && distance < r1 + r2) return 2;
+  });
+
+  return answers.join('\n');
+}
+
+function calcHypotenuse(a, b) {
+  return Math.sqrt(a * a + b * b);
+}
+
+console.log(solve(cases));
