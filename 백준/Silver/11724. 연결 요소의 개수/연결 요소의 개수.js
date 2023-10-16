db0afe9b6ca3cc3cfc8e5d125bc76209862bf419
황성원 <87584033+Magiof@users.noreply.github.com>
2023-10-16 17:20:36 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1144 ms, Memory: 139740 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const [[N, M], ...lines] = fs
+  .readFileSync('dev/stdin')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((e) => e.replace('\r', '').split(' ').map(Number));
+
+function createGraph(M, N, lines) {
+  const graph = Array.from({ length: N + 1 }, () => []);
+
+  for (let [u, v] of lines) {
+    graph[u].push(v);
+    graph[v].push(u);
+  }
+
+  return graph;
+}
+
+function dfs(graph, node, visited) {
+  if (visited[node]) return;
+  visited[node] = true;
+
+  for (const neighbor of graph[node]) {
+    dfs(graph, neighbor, visited);
+  }
+}
+
+function solve(M, N, lines) {
+  const graph = createGraph(M, N, lines);
+
+  let count = 0;
+  const visited = Array(N + 1).fill(false);
+  for (let i = 1; i <= N; i++) {
+    if (!visited[i]) {
+      dfs(graph, Number(i), visited);
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(solve(M, N, lines));
