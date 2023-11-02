68a045c5bed33299c6bf72369841822e45a843d1
seongwon-maumfood <sw9384@balance.site>
2023-11-02 18:14:32 +0900

    solve: 1012

--- CLASS 3++/1012.js
+++ CLASS 3++/1012.js
@@ -6,14 +6,15 @@ let [T, ...input] = fs
   .split('\n')
   .map((e) => e.replaceAll('\r', '').split(' ').map(Number));
 
+const answer = [];
 for (let i = 1; i <= T; i++) {
   const [M, N, K] = input[0];
   const cabbageCoordinates = input.slice(1, K + 1);
-  /**
-   *  배추 밭 획득
-   */
+
   const field = getField(M, N, cabbageCoordinates);
   input = input.slice(K + 1);
+
+  answer.push(countBug(field));
 }
 
 function getField(M, N, cabbageCoordinates) {
@@ -26,7 +27,47 @@ function getField(M, N, cabbageCoordinates) {
 }
 
 function countBug(field) {
-  for (let i = 0; i < field.length; i++) {
-    for (let j = 0; j < field[i].length; j++) {}
+  let visited = new Array(field.length)
+    .fill(false)
+    .map(() => new Array(field[0].length).fill(false));
+  let bugCount = 0;
+
+  function dfs(x, y) {
+    // 방문한 위치를 참으로 설정
+    visited[x][y] = true;
+    // 상하좌우 방향의 좌표를 설정
+    const directions = [
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1],
+    ];
+
+    for (const [dx, dy] of directions) {
+      const newX = x + dx;
+      const newY = y + dy;
+
+      if (
+        newX >= 0 &&
+        newX < field.length &&
+        newY >= 0 &&
+        newY < field[0].length &&
+        field[newX][newY] === 1 &&
+        !visited[newX][newY]
+      )
+        dfs(newX, newY);
+    }
   }
+
+  for (let i = 0; i < field.length; i++) {
+    for (let j = 0; j < field[0].length; j++) {
+      if (field[i][j] === 1 && !visited[i][j]) {
+        bugCount++;
+        dfs(i, j);
+      }
+    }
+  }
+
+  return bugCount;
 }
+console.log(answer.join('\n'));
