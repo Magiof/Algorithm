132ac1604104a95f872dc3a478550c270bef81eb
황성원 <87584033+Magiof@users.noreply.github.com>
2023-11-03 10:28:42 +0900

    [Silver I] Title: Z, Time: 128 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Silver/1074. Z/Z.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const [N, r, c] = fs.readFileSync('dev/stdin').toString().split(' ').map(Number);
+
+function findZVisitOrder(N, r, c) {
+  let count = 0;
+
+  while (N > 0) {
+    const size = Math.pow(2, N - 1);
+    const quadrant = (r < size ? 0 : 2) + (c < size ? 0 : 1);
+
+    count += quadrant * size * size;
+
+    if (r >= size) r -= size;
+    if (c >= size) c -= size;
+    N--;
+  }
+
+  return count;
+}
+
+console.log(findZVisitOrder(N, r, c));
