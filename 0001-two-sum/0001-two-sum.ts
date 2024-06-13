3fd23039acd50cdd4eeb0b2c68fd3ecd71856902
황성원 <87584033+Magiof@users.noreply.github.com>
2024-06-13 15:25:36 +0900

    Time: 98 ms (38.86%), Space: 51.9 MB (87.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,7 +1,6 @@
 function twoSum(nums: number[], target: number): number[] {
     for (let i = 0; i < nums.length; i++) {
         const a = nums[i];
-        if (a >= target) continue;
         for (let j = i + 1; j < nums.length; j++) {
             const b = nums[j]
             if (a + b === target) return [i, j]
